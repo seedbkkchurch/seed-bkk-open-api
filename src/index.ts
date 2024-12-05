cb7b9b39954f15607e96fa567190a5a0b8b28cab
panupongdeve <panupong.dev.web@gmail.com>
2024-12-05 12:03:37 +0000

    add cors api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors({
+    origin: '*'
+  }))
+  .get("/", () => "Hello Elysia")
+  .post("/", (req: any) => {
+    const { body, error } = req
+    if (body?.name === 'tar') {
+      return error(400, 'bad request')
+    }
+
+    return {
+      name: body?.name
+    }
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
